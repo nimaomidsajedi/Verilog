24e3ff4c74a8d1be5d2454967c1aeb6fe1300276
nimaomidsajedi <56468728+nimaomidsajedi@users.noreply.github.com>
2021-08-19 15:05:04 +0200

    Update Bitwise_operators.v

--- Operators/Bitwise_operators.v
+++ Operators/Bitwise_operators.v
@@ -7,7 +7,16 @@ module bitwise_operator ();
       $display("Extend of 8'b1111 is: %b", 8'b1111);
       $display("Extend of 8'b0111 is: %b", 8'b0111);
       $display("Extend of 8'bX111 is: %b", 8'bX111);
-      $display("Extend of 8'bz011 is: %b", 8'bz011);      
+      $display("Extend of 8'bz011 is: %b", 8'bz011);
+      $display("Extend of 3'd180 is: %d", 3'd180);      
+      $display("Extend of 4'd180 is: %d", 4'd180);
+      $display("Extend of 5'd180 is: %d", 5'd180);
+      $display("Extend of 6'd180 is: %d", 6'd180);
+      
+      $display("Extend of 3'b11001100 is: %b", 3'b11001100);      
+      $display("Extend of 4'b11001100 is: %b", 4'b11001100);
+      $display("Extend of 5'b11001100 is: %b", 5'b11001100);
+      $display("Extend of 6'b11001100 is: %b", 6'b11001100);
       $display("---------------");
       $display("AND each bits:");    
       $display("Bitwise AND of 4'b0111 and 4'b1001 is: %b", 4'b0111 & 4'b1001);    
